4e6689515466a0cee382686b8b1bc4458ce2c674
Samuel Legrain <samuel.legrain@bstorm.be>
2024-06-06 08:55:57 +0200

    Ajout correction class "Joueur"

---
+++ Exercice-OO/Models/Joueur.cs
@@ -0,0 +1,23 @@
+﻿using Exercice_OO.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_OO.Models
+{
+    public class Joueur
+    {
+        public string nom;
+        public Pions pion;
+        public int position;
+
+        public bool Avancer()
+        {
+            int[] resultatLance = De.Lancer(2);
+            position += resultatLance[0] + resultatLance[1];
+            return resultatLance[0] == resultatLance[1];
+        }
+    }
+}
--- Exercice-OO/Program.cs
+++ Exercice-OO/Program.cs
@@ -1,4 +1,5 @@
-﻿using Exercice_OO.Models;
+﻿using Exercice_OO.Enums;
+using Exercice_OO.Models;
 
 namespace Exercice_OO
 {
@@ -6,10 +7,24 @@ namespace Exercice_OO
     {
         static void Main(string[] args)
         {
+            /* Test De
             Console.WriteLine("Attention, je lance deux dés !");
             foreach (int resultLance in De.Lancer(2)) {
                 Console.WriteLine($"Un dé me retourne : {resultLance}");
             }
+            */
+
+            Joueur j1 = new Joueur();
+            j1.nom = "Sam";
+            j1.pion = Pions.Dino;
+
+            Console.WriteLine($"Bonjour Joueur 1 ({j1.nom}) : Votre {j1.pion} est à la case {j1.position}.");
+
+            while (j1.Avancer())
+            {
+                Console.WriteLine($"Vous arrivez en case {j1.position} et vous avez fais un double! Rejouez!");
+            }
+            Console.WriteLine($"Vous avez atterri sur la case {j1.position}, laissez votre tour au suivant.");
         }
     }
 }
