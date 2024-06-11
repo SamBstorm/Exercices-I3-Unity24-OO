326d128d8df9509f1ddc808a0f4996383e882797
Samuel Legrain <samuel.legrain@bstorm.be>
2024-06-11 09:20:59 +0200

    Ajout Exercice : Les indexeurs

---
+++ Exercice-OO/Models/Jeu.cs
@@ -0,0 +1,52 @@
+﻿using Exercice_OO.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_OO.Models
+{
+    public class Jeu
+    {
+        private List<Joueur> _joueurs;
+        private List<CasePropriete> _plateau;
+
+        public Joueur[] Joueurs { get {  return _joueurs.ToArray(); } }
+        public CasePropriete[] Plateau { get { return _plateau.ToArray(); } }
+
+        public CasePropriete this[int index]
+        {
+            get {
+                //if (index >= _plateau.Count) return null;
+                //return _plateau[index]; 
+
+                //Permet de faire le tour du plateau
+                return _plateau[index % _plateau.Count];
+            }
+        }
+
+        public Joueur this[Pions pion]
+        {
+            get {
+                foreach (Joueur j in _joueurs)
+                {
+                    if(j.Pion == pion) return j;
+                }
+                return null;
+            }
+        }
+
+        public Jeu(CasePropriete[] plateau)
+        {
+            _plateau = new List<CasePropriete>();
+            _plateau.AddRange(plateau);
+            _joueurs = new List<Joueur>();
+        }
+
+        public void AjouterJoueur(string nom, Pions pion)
+        {
+            if (this[pion] is null) _joueurs.Add(new Joueur(nom, pion));
+        }
+    }
+}
--- Exercice-OO/Program.cs
+++ Exercice-OO/Program.cs
@@ -28,6 +28,7 @@ namespace Exercice_OO
             Console.WriteLine($"Vous avez atterri sur la case {j1.Position}, laissez votre tour au suivant.");
             */
 
+            /* Test class CasePropriete
             CasePropriete premiereCase = new CasePropriete(
                 "Avenue des cerisiers", 
                 Couleurs.Marron, 
@@ -42,8 +43,62 @@ namespace Exercice_OO
             premiereCase.Acheter(j1);
             Console.WriteLine($"Il a actuellement {j1.Proprietes.Length} propriété(s)");
             Console.WriteLine($"Il a actuellement {j1.Solde} Monopoly");
+            */
 
+            Jeu monopoly = new Jeu(
+                [
+                    new CasePropriete("Entrée parc", Couleurs.Marron, 10),
+                    new CasePropriete("Entrée métro", Couleurs.Marron, 12),
+                    new CasePropriete("Cage ascenceur Bât. Gauche", Couleurs.BleuCiel, 14),
+                    new CasePropriete("Cage ascenceur Bât. Droite", Couleurs.BleuCiel, 14),
+                    new CasePropriete("Cage escalier de secours", Couleurs.BleuCiel, 18),
+                    new CasePropriete("Toilette Femme", Couleurs.Violet, 22),
+                    new CasePropriete("Toilette Homme", Couleurs.Violet, 22),
+                    new CasePropriete("Toilette Mixte", Couleurs.Violet, 26),
+                    new CasePropriete("Classe de WEB", Couleurs.Orange, 30),
+                    new CasePropriete("Classe de WAD", Couleurs.Orange, 30),
+                    new CasePropriete("Classe de GAMES", Couleurs.Orange, 32),
+                    new CasePropriete("Réfectoire Bât. Gauche", Couleurs.Rouge, 36),
+                    new CasePropriete("Réfectoire Bât. Droite", Couleurs.Rouge, 36),
+                    new CasePropriete("Réfectaire administration", Couleurs.Rouge, 40),
+                    new CasePropriete("Salle photocopieuses", Couleurs.Jaune, 44),
+                    new CasePropriete("Salle des formateurs", Couleurs.Jaune, 44),
+                    new CasePropriete("Salle de repos", Couleurs.Jaune, 48),
+                    new CasePropriete("Passerelles Niv. 1 et 2", Couleurs.Vert, 52),
+                    new CasePropriete("Passerelles Niv. 3 et 4", Couleurs.Vert, 52),
+                    new CasePropriete("Patio", Couleurs.Vert, 56),
+                    new CasePropriete("Bureau Sonia", Couleurs.Bleu, 60),
+                    new CasePropriete("Bureau Nicole", Couleurs.Bleu, 64)
+                ]);
+            Console.WriteLine("Bienvenu dans Monopoly Interface 3 Le jeu Deluxe Edition SP Lite Turbo");
+            int nbJoueur;
+            do
+            {
+                Console.WriteLine("Combien de joueurs êtes-vous ?");
+            } while (!int.TryParse(Console.ReadLine(), out nbJoueur) || nbJoueur < 2 || nbJoueur > 6);
 
+            for (int i = 0; i < nbJoueur; i++)
+            {
+                Console.WriteLine($"Joueur {i+1} : Quel est votre nom ?");
+                string name = Console.ReadLine();
+                Console.WriteLine($"{name} : Veuillez choisir un Pion :");
+                foreach (string pionName in Enum.GetNames<Pions>())
+                {
+                    Console.WriteLine($"\t- {pionName}");
+                }
+                string pion = Console.ReadLine();
+                monopoly.AjouterJoueur(name, Enum.Parse<Pions>(pion));
+            }
+
+            foreach (Joueur j in monopoly.Joueurs)
+            {
+                j.Avancer();
+                CasePropriete caseActuelle = monopoly[j.Position];
+                Console.WriteLine($"{j.Nom} avance avec son/sa {j.Pion} sur la case {caseActuelle.Nom}");
+                caseActuelle.Acheter(j);
+                Console.WriteLine($"{j.Nom} a actuellement {j.Proprietes.Length} propriété(s)");
+                Console.WriteLine($"{j.Nom} a actuellement {j.Solde} Monopoly");
+            }
         }
     }
 }
